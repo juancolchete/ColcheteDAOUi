06c73f11c5acedc247e653883ac72c64b700acda
juancolchete <juanudkp@gmail.com>
2022-12-18 11:49:17 -0300

    fix: import

--- pages/blog.js
+++ pages/blog.js
@@ -3,7 +3,7 @@ import ColchetePreloader from '../components/global/colchetePreloader';
 import ColcheteHeader from '../components/global/colcheteHeader';
 import ColcheteFooter from '../components/global/colcheteFooter';
 
-import ColcheteBlogTop from '../components/pages/Blog/ColcheteBlogTop';
+import ColcheteBlogTop from '../components/pages/Blog/colcheteBlogTop';
 import ColcheteBlogContent from '../components/pages/Blog/colcheteBlogContent';
 
 export default function Blog() {
